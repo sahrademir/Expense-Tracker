cc771b6cbe1184b294bc2fb88421f7a40ed97e1b
Sahra Demir <sahrademir2005@gmail.com>
2026-07-27 13:50:08 +0300

    feat(auth): implement password hashing and JWT authentication

---
+++ app/auth.py
@@ -0,0 +1,38 @@
+import bcrypt #to hash and verify passwords
+import jwt #after user authentication is using JWT
+import secrets
+from datetime import datetime, timedelta, timezone
+from app.core.config import (
+    SECRET_KEY,
+    ALGORITHM,
+    ACCESS_TOKEN_EXPIRE_MINUTES,
+)
+
+def hash_password(password):
+    password_bytes = password.encode('utf-8') #converting string to bytes
+    salt = bcrypt.gensalt() #add a secure random salt
+    return bcrypt.hashpw(password_bytes,salt).decode('utf-8') #hash the password
+
+def verify_password(password, hashed_password):
+    password_bytes = password.encode('utf-8') #converting string to bytes
+    return bcrypt.checkpw(password_bytes,hashed_password.encode('utf-8')) #compare and return T or F
+
+def create_access_token(data):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=30) #this token expire after 30 mins
+    to_encode.update({"exp": expire})
+    token = jwt.encode(
+        to_encode,
+        SECRET_KEY,
+        algorithm='HS256'
+     )
+    return token
+
+def verify_token(token):
+    payload = jwt.decode(
+        token,
+        SECRET_KEY,
+        algorithms=[ALGORITHM]
+    )
+    return payload
+
